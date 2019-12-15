b570099f1fa4930d40f69e782020aa97de6f0400
scibin <cibinsng@gmail.com>
2019-12-15 21:23:43 +0800

    updated env prod urls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   paypalClientID: 'AVkl-fDIc7GqqCLgsvFB8tKXG02iMnW-GIUUggLXZZSvqgRxtGO_PuqINl8-9B0s2j7LCDIYkavxwGHI',
-  WS_SERVER_URL: 'ws://localhost:3000',
-  baseURL: 'http://localhost:3000'
+  WS_SERVER_URL: 'ws://trinance.herokuapp.com',
+  baseURL: 'https://trinance.herokuapp.com'
 };
